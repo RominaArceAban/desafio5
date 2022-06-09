fbe266c3ea769482063d82fc02d60796f800cc30
Romina Arce Aban <rarceaban@outlook.com>
2022-06-08 23:39:01 -0300

    firs commmit

---
+++ webServer.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+const PORT = 8080
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const productos = []
+
+app.get('/', (req, res) => {
+    res.render('indexVista', { productos })
+})
+app.post('/productos', (req, res) => {
+    productos.push(req.body)
+    console.log(req.body, 'added to product')
+
+    res.render('indexFormulario', { productos })
+})
+
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ' + PORT)
+})
